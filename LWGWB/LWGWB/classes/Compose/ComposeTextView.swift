366f1a480d5f3d47f95b49f2ceb93d31f6e64169
lwg123 <656442711@qq.com>
2017-05-03 11:12:44 +0800

    update

---
+++ LWGWB/LWGWB/classes/Compose/ComposeTextView.swift
@@ -0,0 +1,43 @@
+//
+//  ComposeTextView.swift
+//  LWGWB
+//
+//  Created by weiguang on 2017/5/3.
+//  Copyright © 2017年 weiguang. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ComposeTextView: UITextView {
+
+    lazy var placeHolderLabel: UILabel = UILabel()
+    
+    // 自定义控件，如果需要添加子控件，一般在这个方法中进行
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setupUI()
+    }
+    
+    // 如果需要初始化子控件，或者对子控件进行布局在此方法中进行
+    override func awakeFromNib() {
+        
+    }
+
+}
+
+extension ComposeTextView {
+    fileprivate func setupUI() {
+        addSubview(placeHolderLabel)
+        placeHolderLabel.snp_makeConstraints { (make) in
+            make.top.equalTo(8)
+            make.left.equalTo(10)
+            
+            placeHolderLabel.textColor = UIColor.lightGray
+            placeHolderLabel.font = font
+            placeHolderLabel.text = "分享新鲜事..."
+            textContainerInset = UIEdgeInsetsMake(8, 7, 0, 7)
+        }
+    }
+}
